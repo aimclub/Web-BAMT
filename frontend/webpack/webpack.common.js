575558ccf0e0d057dd49b18927ab766074f4773e
Anastasia Tsibernaya <anastasiacibernaja@yandex.ru>
2022-07-01 15:40:38 +0300

    change build folder name

--- frontend/webpack/webpack.common.js
+++ frontend/webpack/webpack.common.js
@@ -5,7 +5,7 @@ const HTMLWebpackPlugin = require("html-webpack-plugin");
 module.exports = {
   entry: path.resolve(__dirname, "..", "./src/index.tsx"),
   output: {
-    path: path.resolve(__dirname, "..", "./prod"),
+    path: path.resolve(__dirname, "..", "./build"),
     filename: "[name].[hash].js",
     publicPath: "/",
   },
