a9dbafc7270d1810971c47adce293b1adccee1cb
Roman223 <romius2001@gmail.com>
2023-05-26 16:10:26 +0400

    manager tests freezed (see comments)

--- tests/test_manager.py
+++ tests/test_manager.py
@@ -1,23 +1,23 @@
-from app.api.experiment.service import Sampler, Manager
-from bamt_special.networks.hybrid_bn import HybridBN
-
-import pytest
-
-
-@pytest.fixture()
-def manager(client):
-    bn_meta = client.get("api/bn_manager/get_BN/test").json["networks"]["0"]
-
-    bn = HybridBN(has_logit=bn_meta["has_logit"], use_mixture=bn_meta["use_mixture"])
-    bn.load("test_bn.json")
-
-    sampler = Sampler(bn)
-    sample = sampler.sample(10)
-
-    return Manager(bn=bn, sample=sample, owner="test", net_name="test", dataset_name="test_dataset")
-
 # The tests below were frozen due to the occurrence of non-pickle serialization.
 # Most likely, because of version conflict, resolved on May 24.
+
+# from app.api.experiment.service import Sampler, Manager
+# from bamt_special.networks.hybrid_bn import HybridBN
+#
+# import pytest
+#
+#
+# @pytest.fixture()
+# def manager(client):
+#     bn_meta = client.get("api/bn_manager/get_BN/test").json["networks"]["0"]
+#
+#     bn = HybridBN(has_logit=bn_meta["has_logit"], use_mixture=bn_meta["use_mixture"])
+#     bn.load("test_bn.json")
+#
+#     sampler = Sampler()
+#     sample = sampler.sample(10, bn)
+#
+#     return Manager(bn=bn, samples=sample, owner="test", net_name="test", dataset_name="test_dataset")
 
 # def test_save_sample(app, client, manager):
 #     with app.app_context():
